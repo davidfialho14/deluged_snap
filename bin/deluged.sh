69b415c911d29f70cedaa1f625be5421e8505259
David Fialho <fialho.david@gmail.com>
2017-06-07 19:55:14 +0100

    Init script now creates directories and files required by deluged
    This fixes issues with warnings indicating files and directories were missing

--- bin/deluged.sh
+++ bin/deluged.sh
@@ -3,6 +3,14 @@
 # Removes locale warnings
 export LC_ALL=C
 
+# We noticed deluged was trying to access the '.config/deluge/state' directory to
+# write some data into it, but it was not able to do so because the directory
+# was missing. Creating the directory solved the issue.
+mkdir -p $SNAP_USER_DATA/.config/deluge/state
+
+# Create the auth file which will contain the registered users
+touch $SNAP_USER_DATA/.config/deluge/auth
+
 # The -d flag corresponds to the --no-daemonize option
 # Here we let the snap system handle the daemonizing process
 # This approach allows the output of deluged to be directed to systemd and,

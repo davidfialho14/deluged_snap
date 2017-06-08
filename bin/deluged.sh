c19259f88a160bea14a3938613fb66acdd2bad05
David Fialho <fialho.david@protonmail.com>
2017-06-08 16:59:48 +0100

    Deluged now uses SNAP_DATA as hoem directory

--- bin/deluged.sh
+++ bin/deluged.sh
@@ -5,12 +5,22 @@
 # Removes locale warnings
 export LC_ALL=C
 
+# The deluge daemon creates directories containing configurations and the
+# downloads inside teh user's home directory
+# Here the daemon runs with the root user and therefore HOME will point to the
+# root snap home directory (directory pointed by SNAP_USER_DATA for root user)
+# At the time, after removing a snap, the data place in the root snap home
+# directory is not deleted and the documentation suggests using the SNAP_DATA
+# directory to place data from long running services as the deluge daemon
+# Because of this, we set the root home directory to the SNAP_DATA directory
+export HOME=$SNAP_DATA
+
 # We noticed deluged was trying to access the '.config/deluge/state' directory to
 # write some data into it, but it was not able to do so because the directory
 # was missing. Creating the directory solved the issue.
-mkdir -p $SNAP_USER_DATA/.config/deluge/state
-touch $SNAP_USER_DATA/.config/deluge/state/torrents.state
-touch $SNAP_USER_DATA/.config/deluge/state/torrents.fastresume
+mkdir -p $SNAP_DATA/.config/deluge/state
+touch $SNAP_DATA/.config/deluge/state/torrents.state
+touch $SNAP_DATA/.config/deluge/state/torrents.fastresume
 
 # The auth file is used by deluge to store the users. The daemon accesses this
 # file to check the username and password of each user trying to login
